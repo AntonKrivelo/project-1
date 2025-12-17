89e2a7928dcc333a32c1ae043ea12e1157e76299
Anton Krivelo <krivelo2017@mail.ru>
2025-12-17 21:46:52 +0300

    connect to DB

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const sequelize = require('./db');
+
+const PORT = process.env.PORT || 4000;
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => {
+      console.log(`The server is running on the port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+app.get('/', (req, res) => {
+  res.send('The server is running!');
+});
+
+start();
